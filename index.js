7a8e95dc1cdb02e1695ae241da5a5e98b8d7283f
triciamedina <tricia.medina@gmail.com>
2021-01-31 00:17:35 -0800

    Mobile nav styling and functionality

--- index.js
+++ index.js
@@ -1,7 +1,37 @@
 "use strict";
 
-$(function() {
+function handleMenuToggle() {
+    $(document).on("click", "#menu-btn", function(e) {
+        e.preventDefault();
+        $("#nav-list").toggleClass("visible")
+    });
 
+    $(document).on("click", "#strategies", function(e) {
+        e.preventDefault();
+        $("#strategies-subnav").toggleClass("visible")
+    });
+
+    $(document).on("click", "#growth", function(e) {
+        e.preventDefault();
+        $("#growth-subnav").toggleClass("visible")
+    });
+
+    $(document).on("click", "#resources", function(e) {
+        e.preventDefault();
+        $("#resources-subnav").toggleClass("visible")
+    });
+
+    $(document).on("click", "#articles", function(e) {
+        e.preventDefault();
+        $("#articles-subnav").toggleClass("visible")
+    });
+}
+
+function loadNav() {
     $("#nav-bar-container").load("components/navigation.html");
-    
+}
+
+$(function() {
+    loadNav();
+    handleMenuToggle();
 });
