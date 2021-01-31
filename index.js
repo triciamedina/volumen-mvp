5534a5082b38b5bdc020445a4884ae785edd4e50
triciamedina <tricia.medina@gmail.com>
2021-01-31 10:35:48 -0800

    Remove unused js

--- index.js
+++ index.js
@@ -1,37 +1,9 @@
 "use strict";
 
-function handleMenuToggle() {
-    $(document).on("click", "#menu-btn", function(e) {
-        e.preventDefault();
-        $("#nav-list").toggleClass("visible")
-    });
-
-    $(document).on("click", "#strategies", function(e) {
-        e.preventDefault();
-        $("#strategies-subnav").toggleClass("visible")
-    });
-
-    $(document).on("click", "#growth", function(e) {
-        e.preventDefault();
-        $("#growth-subnav").toggleClass("visible")
-    });
-
-    $(document).on("click", "#resources", function(e) {
-        e.preventDefault();
-        $("#resources-subnav").toggleClass("visible")
-    });
-
-    $(document).on("click", "#articles", function(e) {
-        e.preventDefault();
-        $("#articles-subnav").toggleClass("visible")
-    });
-}
-
 function loadNav() {
     $("#nav-bar-container").load("components/navigation.html");
 }
 
 $(function() {
     loadNav();
-    // handleMenuToggle();
 });
